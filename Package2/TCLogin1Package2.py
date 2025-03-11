bc9b584e5e51a87473d1100278a06ee8d2035784
Amol <adchavan28@gmail.com>
2025-03-11 20:18:11 +0530

    this my first commit for all files

---
+++ Package2/TCLogin1Package2.py
@@ -0,0 +1,15 @@
+import unittest
+
+class loginFourth  (unittest.TestCase):
+    def testMethod1(self):
+        print("this is loginFourth method 1")
+
+    def testMethod2(self):
+        print("this is loginFourth method 2")
+
+    def testMethod3(self):
+        print("this is loginFourth method 3")
+
+
+if __name__== "__main__":
+    unittest.main()
